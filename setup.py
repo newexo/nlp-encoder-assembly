be54cba9113ccc34ecdf50164587fa1d55e1e87f
Reuben Brasher <rkbrasher@gmail.com>
2018-11-21 12:09:50 -0800

    Simplify requirements and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+"""
+setup.py file for dependencies module
+"""
+
+from distutils.core import setup
+
+setup (name     = 'nlp_encoder_assembly',
+    version     = '0.1',
+    author      = "Gus Peterson",
+    description = """NLP Encoders and Decoders in Keras""")
