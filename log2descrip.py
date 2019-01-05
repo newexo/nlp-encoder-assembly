ce813f1f3b5637a815acca344ab7d5a525011862
gustable <peterson.gus@gmail.com>
2019-01-05 11:19:21 -0800

    Objectivize the mudlog parsing into a py file

--- log2descrip.py
+++ log2descrip.py
@@ -23,39 +23,41 @@ def pull_logs(path_string):
 
 #Expects a list of log lines
 def filter_desc(log_doc):
+	buff = [json.loads(line) for line in document[:]]
 	description = []
 	index = -1
 	indices = []
 	descripDict = {}
-	for line in log_doc: #in the end, append all good lines to a list then dict
-	    ansi_rem = re.split(r'(37m)|(33m)', line, 100)
-	    stripFront = ansi_rem
-	    l = len(ansi_rem)
-	    #print(stripFront[l-1])
-	    ansi_rem2 = re.split(r'(\\)', stripFront[l-1], 100)
-	    stripBack = ansi_rem2
-	    a = stripBack[0]
-	    l = len(a)
-	    if (a.startswith('{"mud": "sloth", "line": "') ):
-	        a = a[26:(l-74)]
-	    if (a.startswith('", "tag": "mudline",') ):
-	        a = ''
-	    if (a.startswith('",') ):
-	        a = ''
-	    if (a.startswith('"}') ):
-	        a = ''
-	    if (a.startswith('***') ):
-	        a = '' 
-	    if ('timestamp' in a):
-	        a = ''
-	    if ('http' in a):
-	        a = ''
-	    if (a != ''):
-	        #print(a) #for nice output
-	        description.append(a)
-	    index += 1
-	    indices.append(index)
-	descripDict = dict(zip(indices, description))
+    descr = [d for d in tenk if d['tag'] == 'mudline']
+    for l in descr[:]:
+	    li = l['line']
+	    ansi_rem = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~](\\?\;?\d+)*m')
+	    p = ansi_rem.sub('', str(li))
+	    print(p) #for nice output
+		description.append(a)
+		index += 1
+		indices.append(index)
+		descripDict = dict(zip(indices, description))
 
 	#print(index) #for number of lines
 	return descripDict
+
+
+def filter_actions(log_doc):
+	buff = [json.loads(line) for line in document[:]]
+	action = []
+	index = -1
+	indices = []
+	actionDict = {}
+	act = [d for d in tenk if d['tag'] == 'playerline']
+	for l in act[:]:
+	    li = l['line']
+	    ansi_rem = re.compile(r'\x1B\[[0-?]*[ -/]*[@-~](\\?\;?\d+)*m')
+	    p = ansi_rem.sub('', str(li))
+	    print(p)
+		action.append(a)
+		index += 1
+		indices.append(index)
+		actionDict = dict(zip(indices, action))	    
+	#print(index) #for number of lines
+	return actionDict	
