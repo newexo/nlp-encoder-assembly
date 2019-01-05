27e0ac954dc2d9ab8b7aad323940b215115648b6
Reuben Brasher <rkbrasher@gmail.com>
2019-01-04 16:45:51 -0800

    Have default names for layers

--- text_encoder.py
+++ text_encoder.py
@@ -1,10 +1,19 @@
-import hyper_params
+import hyper_params as hp
 
 class Hyper(vae.Hyper):
     def __init__(self, embedder, layers):
         self.embedder = embedder
         self.layers = layers
 
+    @property
+    def all_layers(self):
+        return [self.embedder] + self.layers
+    
+
+    @property
+    def default_name(self):
+        return 'encoder'
+    
     @property
     def vocab_size(self):
         return self.embedder.vocab_size
@@ -20,4 +29,13 @@ class Hyper(vae.Hyper):
     @property
     def return_sequences(self):
         return self.encoding._return_sequences
+
+    def display(self):
+        self.embedder.display()
+        for layer in self.layers:
+            layer.display()
     
+    def make_layers(self, name='encoder'):
+        prefix = hp.name_prefix(name)
+        return [layer.make_layer(layer) for layer in self.all_layers]
+        
